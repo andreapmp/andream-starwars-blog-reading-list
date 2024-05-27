f7625b318041a127c1d76b84636ed801e0bd8641
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-05-27 01:30:34 +0000

    Navbar changes

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+				<img
+					src="https://seeklogo.com/images/S/Star_Wars-logo-97DD55947B-seeklogo.com.png"
+					alt="Star Wars Logo"
+					className="mr-2"
+					style={{ height: "60px" }} 
+				/>
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary">Favorites</button>
 				</Link>
 			</div>
 		</nav>
